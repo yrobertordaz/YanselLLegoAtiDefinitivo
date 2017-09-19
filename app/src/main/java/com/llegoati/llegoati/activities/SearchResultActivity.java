38514ae12fb11c985238527ab77887b6f1e46ca0
Yansel Robert Ordaz <yrobertordaz@gmail.com>
2017-09-19 11:48:42 -0400

    1. Go shopping cart in Product List by category > Product Detail ==> fixed. 2. Go shopping cart in Search Result > Product Detail ==> fixed.

--- app/src/main/java/com/llegoati/llegoati/activities/SearchResultActivity.java
+++ app/src/main/java/com/llegoati/llegoati/activities/SearchResultActivity.java
@@ -5,9 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
@@ -125,6 +123,14 @@ public class SearchResultActivity extends BaseActivity implements SwipeRefreshLa
     @Override
     protected void onNewIntent(Intent intent) {
         handleIntent(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        if (MainActivity.goShoppingCart) {
+            finish();
+        }
+        super.onResume();
     }
 
     @Override
